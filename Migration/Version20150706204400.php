cf91e1285ea4d0503db408a9d26ffe8f451030f0
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2015-08-20 17:57:03 +0900

    fix typo

--- Migration/Version20150706204400.php
+++ Migration/Version20150706204400.php
@@ -14,12 +14,12 @@ class Version20150706204400 extends AbstractMigration
 
     public function down(Schema $schema)
     {
-        $schema->dropTable('plg_category_contnet');
+        $schema->dropTable('plg_category_content');
     }
 
     protected function createPluginTable(Schema $schema)
     {
-        $table = $schema->createTable("plg_category_contnet");
+        $table = $schema->createTable("plg_category_content");
         $table->addColumn('category_id', 'integer');
         $table->addColumn('content', 'text', array('notnull' => false));
         $table->setPrimaryKey(array('category_id'));
