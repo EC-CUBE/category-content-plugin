0a9454aee5f3ad4f93cb51f404ba593eab1416bb
hoand <ndhoa@lockon-vn.com>
2016-10-21 17:29:34 +0700

    - For Unittest

---
+++ Tests/Web/FrontTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Plugin\CategoryContent\Tests\Web;
+
+use Eccube\Tests\Web\AbstractWebTestCase;
+use Plugin\CategoryContent\Entity\CategoryContent;
+use Symfony\Component\DomCrawler\Crawler;
+
+const CATEGORY_CONTENT = 'テストカテゴリコンテンツ';
+const CATEGORY_ID = 3;
+
+/**
+ * Class FrontTest
+ * @package Plugin\CategoryContent\Tests\Web
+ */
+class FrontTest extends AbstractWebTestCase
+{
+    /**
+     * Setup
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->addCategoryContent(CATEGORY_ID, CATEGORY_CONTENT);
+    }
+
+    /**
+     * カテゴリ画面のルーティング
+     */
+    public function testRoutingProduct()
+    {
+        $crawler = $this->client->request(
+            'GET',
+            $this->app->url('product_list', array('category_id' => CATEGORY_ID))
+        );
+        $this->assertContains(CATEGORY_CONTENT, $crawler->html());
+    }
+
+    /**
+     * addCategoryContent
+     *
+     * @param $id
+     * @param $content
+     */
+    private function addCategoryContent($id, $content)
+    {
+        $CategoryContent = new CategoryContent();
+        $CategoryContent
+            ->setId($id)
+            ->setContent($content);
+        $this->app['orm.em']->persist($CategoryContent);
+        $this->app['orm.em']->flush($CategoryContent);
+    }
+}
