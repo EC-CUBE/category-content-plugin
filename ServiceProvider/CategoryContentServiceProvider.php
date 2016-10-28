6e9e3d9879b82783e1aa709edd0ff87396a7a80b
hoand <ndhoa@lockon-vn.com>
2016-10-28 09:39:35 +0700

    - social tool

--- ServiceProvider/CategoryContentServiceProvider.php
+++ ServiceProvider/CategoryContentServiceProvider.php
@@ -27,13 +27,20 @@ class CategoryContentServiceProvider implements ServiceProviderInterface
      */
     public function register(BaseApplication $app)
     {
-        // Form/Extension
-        $app['form.type.extensions'] = $app->share($app->extend('form.type.extensions', function ($extensions) use ($app) {
-            $extensions[] = new CategoryContentExtension($app['config'], $app);
+        // @deprecated for since v3.0.0, to be removed in 3.1.
+        if (version_compare(Constant::VERSION, '3.0.9', '<')) {
+            // Form/Extension
+            $app['form.type.extensions'] = $app->share(
+                $app->extend(
+                    'form.type.extensions',
+                    function ($extensions) {
+                        $extensions[] = new CategoryContentExtension();
 
-            return $extensions;
-        }));
-
+                        return $extensions;
+                    }
+                )
+            );
+        }
         //Repository
         $app['category_content.repository.category_content'] = $app->share(function () use ($app) {
             return $app['orm.em']->getRepository('Plugin\CategoryContent\Entity\CategoryContent');
