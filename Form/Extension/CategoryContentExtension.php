4c03a02c59fef843eaf6dc2134753d227c977634
hoand <ndhoa@lockon-vn.com>
2016-12-06 15:20:49 +0700

    Merge with master

--- Form/Extension/CategoryContentExtension.php
+++ Form/Extension/CategoryContentExtension.php
@@ -1,27 +1,46 @@
 <?php
 /*
- * This file is part of EC-CUBE
- *
- * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
- * http://www.lockon.co.jp/
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * This file is part of the CategoryContent plugin
+  *
+  * Copyright (C) 2016 LOCKON CO.,LTD. All Rights Reserved.
+  *
+  * For the full copyright and license information, please view the LICENSE
+  * file that was distributed with this source code.
+  */
 
 namespace Plugin\CategoryContent\Form\Extension;
 
+use Eccube\Application;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * Class CategoryContentExtension
- *
- * @package Plugin\CategoryContent\Form\Extension
- * @deprecated for since v3.0.0, to be removed in 3.1.
+ * Class CategoryContentExtension.
  */
 class CategoryContentExtension extends AbstractTypeExtension
 {
+    /**
+     * @var Application
+     */
+    private $app;
+
+    /**
+     * CategoryContentExtension constructor.
+     *
+     * @param object $app
+     */
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * buildForm.
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -31,10 +50,25 @@ class CategoryContentExtension extends AbstractTypeExtension
                 array(
                     'label' => 'カテゴリ別表示用コンテンツ',
                     'mapped' => false,
+                    'required' => false,
+                    'constraints' => array(
+                        new Assert\Length(array(
+                            'max' => $this->app['config']['category_text_area_len'],
+                        )),
+                    ),
+                    'attr' => array(
+                        'maxlength' => $this->app['config']['category_text_area_len'],
+                        'placeholder' => $this->app->trans('admin.plugin.category.content'),
+                    ),
                 )
             );
     }
 
+    /**
+     * getExtendedType.
+     *
+     * @return string
+     */
     public function getExtendedType()
     {
         return 'admin_category';
--- Resource/doctrine/migration/Version20150706204400.php
+++ Resource/doctrine/migration/Version20150706204400.php
@@ -1,62 +1,68 @@
 <?php
 /*
- * This file is part of EC-CUBE
- *
- * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
- * http://www.lockon.co.jp/
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * This file is part of the CategoryContent plugin
+  *
+  * Copyright (C) 2016 LOCKON CO.,LTD. All Rights Reserved.
+  *
+  * For the full copyright and license information, please view the LICENSE
+  * file that was distributed with this source code.
+  */
 
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\SchemaTool;
-use Eccube\Common\Constant;
+use Eccube\Application;
+use Doctrine\ORM\EntityManager;
+use Plugin\CategoryContent\Util\Version;
 
+/**
+ * Class Version20150706204400.
+ */
 class Version20150706204400 extends AbstractMigration
 {
+    /**
+     * @var string table name
+     */
+    const NAME = 'plg_category_content';
+
     protected $entities = array(
         'Plugin\CategoryContent\Entity\CategoryContent',
     );
 
+    /**
+     * Setup data.
+     *
+     * @param Schema $schema
+     */
     public function up(Schema $schema)
     {
-        if (version_compare(Constant::VERSION, '3.0.9', '>=')) {
-            // 3,0,9 以降の場合, dcm.ymlの定義からテーブル生成を行う.
-            $app = \Eccube\Application::getInstance();
-            $meta = $this->getMetadata($app['orm.em']);
-            $tool = new SchemaTool($app['orm.em']);
-            $tool->createSchema($meta);
+        if (Version::isSupportGetInstanceFunction()) {
+            $this->createCategoryContent($schema);
         } else {
-            // 3.0.0 - 3.0.8
-            $table = $schema->createTable("plg_category_content");
-            $table->addColumn('category_id', 'integer');
-            $table->addColumn('content', 'text', array('notnull' => false));
-            $table->setPrimaryKey(array('category_id'));
+            $this->createCategoryContentForOldVersion($schema);
         }
     }
 
+    /**
+     * Remove data.
+     *
+     * @param Schema $schema
+     */
     public function down(Schema $schema)
     {
-        if (version_compare(Constant::VERSION, '3.0.9', '>=')) {
-            // 3,0,9 以降の場合, dcm.ymlの定義からテーブル/シーケンスの削除を行う
-            $app = \Eccube\Application::getInstance();
+        if (Version::isSupportGetInstanceFunction()) {
+            $app = Application::getInstance();
             $meta = $this->getMetadata($app['orm.em']);
-
             $tool = new SchemaTool($app['orm.em']);
             $schemaFromMetadata = $tool->getSchemaFromMetadata($meta);
-
             // テーブル削除
             foreach ($schemaFromMetadata->getTables() as $table) {
                 if ($schema->hasTable($table->getName())) {
                     $schema->dropTable($table->getName());
                 }
             }
-
             // シーケンス削除
             foreach ($schemaFromMetadata->getSequences() as $sequence) {
                 if ($schema->hasSequence($sequence->getName())) {
@@ -64,11 +70,61 @@ class Version20150706204400 extends AbstractMigration
                 }
             }
         } else {
-            // 3.0.0 - 3.0.8
-            $schema->dropTable('plg_category_content');
+            // this down() migration is auto-generated, please modify it to your needs
+            $schema->dropTable(self::NAME);
         }
     }
 
+    /**
+     * Create recommend table.
+     *
+     * @param Schema $schema
+     *
+     * @return bool
+     */
+    protected function createCategoryContent(Schema $schema)
+    {
+        if ($schema->hasTable(self::NAME)) {
+            return true;
+        }
+
+        $app = Application::getInstance();
+        $em = $app['orm.em'];
+        $classes = array(
+            $em->getClassMetadata('Plugin\CategoryContent\Entity\CategoryContent'),
+        );
+        $tool = new SchemaTool($em);
+        $tool->createSchema($classes);
+
+        return true;
+    }
+
+    /**
+     * おすすめ商品テーブル作成.
+     *
+     * @param Schema $schema
+     */
+    protected function createCategoryContentForOldVersion(Schema $schema)
+    {
+        $table = $schema->createTable(self::NAME);
+        $table->addColumn('category_id', 'integer', array(
+            'notnull' => true,
+        ));
+
+        $table->addColumn('content', 'text', array(
+            'notnull' => false,
+        ));
+
+        $table->setPrimaryKey(array('category_id'));
+    }
+
+    /**
+     * Get metadata.
+     *
+     * @param EntityManager $em
+     *
+     * @return array
+     */
     protected function getMetadata(EntityManager $em)
     {
         $meta = array();
